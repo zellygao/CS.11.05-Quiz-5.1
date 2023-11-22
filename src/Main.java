051bc02c0d33f8e5a95b7aa4114f5a291f444834
zellygao <zg2311@student.cis.edu.hk>
2023-11-22 11:39:49 +0800

    Removed redundant comments.

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,14 @@ public class Main {
      * zip(myArray1, myArray2) → {1,3,5,7,9,2,4,6,8,10,12,14,16}
      */
     public static int[] combine(int[] array1, int[] array2) {
-
-        return null;
+        int[] array = new int [array1.length+array2.length];
+        for(int i = 0;i<array1.length; i++){
+            array[i]+=array1[i];
+        }
+        for(int i = 0;i<array2.length; i++){
+            array[i+array1.length]+=array2[i];
+        }
+        return array;
 
     }
 
@@ -21,9 +27,14 @@ public class Main {
      * zip(myArray1, myArray2) → {1,2,3,4,5,6,7,8,9,10}
      */
     public static int[] zip(int[] array1, int[] array2) {
-
-        return null;
-
+        int[] array = new int [array1.length*2];
+        for(int i =0; i<array.length-2; i+=2){
+            for(int k = 0; k<array1.length; k++){
+                array[i] = array1[k];
+                array[i+1] = array2[k];
+            }
+        }
+        return array;
     }
 
     /**
@@ -34,8 +45,11 @@ public class Main {
      * product(myArray1, myArray2) → {2,12,30,56,90}
      */
     public static int[] product(int[] array1, int[] array2) {
-
-        return null;
+        int[] array = new int[array1.length];
+        for(int i=0; i<array1.length;i++){
+            array[i] = array1[i] * array2[i];
+        }
+        return array;
 
     }
 
@@ -56,14 +70,25 @@ public class Main {
      * capitalCount(words) → {1, 2, 2, 0}
      */
     public static int[] capitalCount(String[] words) {
-
-        return null;
+        int[] array = new int [words.length];
+        for(int i =0; i<words.length; i++){
+            array[i] = countCapitalLetters(words[i]);
+        }
+        return array;
 
     }
 
     public static int countCapitalLetters(String word) {
-
-        return 0;
+        int count = 0;
+        for(int i =0; i<word.length(); i++){
+            if(Character.isAlphabetic(word.charAt(i))){
+                int code = (int) word.charAt(i);
+                if(code>=65 && code<=90){
+                    count++;
+                }
+            }
+        }
+        return count;
 
     }
 
